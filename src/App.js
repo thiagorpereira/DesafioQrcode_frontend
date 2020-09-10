0915aefc568c165abcc790b3476568ab4e386a63
Thiago Pereira <thiagorpereira7@gmail.com>
2020-09-10 14:20:22 -0300

    HTML of form and set input values in state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 
 function App() {
+
+
+  const [formData, setFormData] = useState({
+    payerIdentifier: '',
+    value: '',
+    recieverIdentifier: '',
+  })
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+
+    })
+  }
+
   return (
-    <h1>Hello Word</h1>
+    <form>
+      <div className="field">
+        <label htmlFor="payerIdentifier">Identificação do Pagador</label>
+        <input 
+          type="text"
+          name="payerIdentifier"
+          id="payerIdentifier"
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="field">
+        <label htmlFor="value">Valor da Transferencia</label>
+        <input 
+          type="text"
+          name="value"
+          id="value"
+          onChange={handleInputChange}
+        />
+      </div>
+      <div className="field">
+        <label htmlFor="recieverIdentifier">Identificação do Recebedor</label>
+        <input 
+          type="text"
+          name="recieverIdentifier"
+          id="recieverIdentifier"
+          onChange={handleInputChange}
+        />
+      </div>
+    </form>
   );
 }
 
